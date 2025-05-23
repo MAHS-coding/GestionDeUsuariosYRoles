1a13ba8f6a7839b87fbff89876d6eee53d333af8
MAHSCODING <mahscoding@gmail.com>
2025-05-23 19:52:04 -0400

    first commit

---
+++ src/main/java/com/Microservicio/GestionDeUsuariosYRoles/GestionDeUsuariosYRolesApplication.java
@@ -0,0 +1,13 @@
+package com.Microservicio.GestionDeUsuariosYRoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeUsuariosYRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeUsuariosYRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Microservicio/GestionDeUsuariosYRoles/GestionDeUsuariosYRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservicio.GestionDeUsuariosYRoles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeUsuariosYRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
