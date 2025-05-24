121dc8bb2a1b1845688a103e101d41ec4c5fd1f6
MAHSCODING <mahscoding@gmail.com>
2025-05-23 23:23:50 -0400

    no se, no hice nada

---
+++ src/main/java/com/Microservicio/GestionDeUsuariosYRoles/model/Usertype.java
@@ -0,0 +1,7 @@
+package com.Microservicio.GestionDeUsuariosYRoles.model;
+
+public enum Usertype {
+    ADMINISTRADOR,
+    PROFESOR,
+    ESTUDIANTE
+}
---
+++ src/main/java/com/Microservicio/GestionDeUsuariosYRoles/model/Usuario.java
@@ -0,0 +1,49 @@
+package com.Microservicio.GestionDeUsuariosYRoles.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String nombre;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Enumerated(EnumType.STRING)
+    private UserType tipoUsuario;
+    
+    private boolean activo = true;
+    
+    @ManyToMany
+    @JoinTable(
+        name = "usuario_roles",
+        joinColumns = @JoinColumn(name = "usuario_id"),
+        inverseJoinColumns = @JoinColumn(name = "rol_id")
+    )
+    private Set<Rol> roles = new HashSet<>();
+}
